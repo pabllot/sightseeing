1d24215e421e02d498b378b9d3097d0b58fe8fa4
Pabllot07 <pabllot07@outlook.com>
2023-04-30 16:07:24 -0300

    config img

--- src/config/categories.ts
+++ src/config/categories.ts
@@ -1,1 +1,15 @@
-export const categories = ["Chocolates", "Restaurantes", "Museus", "Turismo", "Lojas", "Parques"];
+import chocolate from "../assets/chocolate.jpg";
+import parques from "../assets/parques.jpg";
+import restaurantes from "../assets/restaurantes.jpg";
+import museus from "../assets/museus.jpeg";
+import lojas from "../assets/lojas.jpg";
+import turismo from "../assets/turismo.jpg";
+
+export const categories = [
+  { category: "Parques", image: parques },
+  { category: "Restaurantes", image: restaurantes },
+  { category: "Museus", image: museus },
+  { category: "Chocolates", image: chocolate },
+  { category: "Lojas", image: lojas },
+  { category: "Turismo", image: turismo },
+];
